269a99bbe1d887b10944b091abc57222c9fa275a
Anamikaghosh18 <g.anamika0101@gmail.com>
2024-06-03 09:51:03 +0530

    update

--- README.md
+++
@@ -1,7 +0,0 @@
-# CodeClauseInternship_MyDigitalPresence
-<h2>
-This is a personal portfolio website to showcasing the skills and projects </h2>
-<br>
-<h1> Description</h1>
-<br>
-This is a portfolio site containing an about me section, projects gallery , and contact form.
---
+++ main/script.js
@@ -0,0 +1,21 @@
+function visitGitHub() {
+    window.open("https://github.com/Anamikaghosh18","_blank");
+}
+function visitGmail() {
+    window.open("https://gmail.com/g.anamika0101", "_blank");
+}
+function visitLinkedin() {
+    window.open("https://linkedin.com/anamikaghosh18", "_blank");
+}
+/*function visitInstagram() {
+    window.open("https://instagram.com/__imanamika", "_blank");
+}
+function visitFacebook() {
+    window.open("https://github.com/Anamikaghosh", "_blank");
+}*/
+function visitTwitter() {
+    window.open("https://twitter.com/__imanamika", "_blank");
+}
+function toggleBudget(show) {
+    document.getElementById('budget-container').style.display = show ? 'block' : 'none';
+}
