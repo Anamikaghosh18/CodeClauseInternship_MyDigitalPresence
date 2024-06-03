c952e84cbe81e06ceca8cd58fc841d87a354ac1c
Anamika Ghosh <166281527+Anamikaghosh18@users.noreply.github.com>
2024-06-03 16:06:42 +0530

    Update script.js

--- script.js
+++ script.js
@@ -7,15 +7,21 @@ function visitGmail() {
 function visitLinkedin() {
     window.open("https://linkedin.com/anamikaghosh18", "_blank");
 }
-/*function visitInstagram() {
-    window.open("https://instagram.com/__imanamika", "_blank");
-}
-function visitFacebook() {
-    window.open("https://github.com/Anamikaghosh", "_blank");
-}*/
 function visitTwitter() {
     window.open("https://twitter.com/__imanamika", "_blank");
 }
 function toggleBudget(show) {
     document.getElementById('budget-container').style.display = show ? 'block' : 'none';
 }
+function addSkill() {
+            const skillInput = document.getElementById('skill-input');
+            const skill = skillInput.value.trim();
+            if (skill) {
+                const skillsContainer = document.getElementById('skills-container');
+                const skillBox = document.createElement('div');
+                skillBox.className = 'skill-box';
+                skillBox.innerText = skill;
+                skillsContainer.appendChild(skillBox);
+                skillInput.value = '';
+            }
+        }
